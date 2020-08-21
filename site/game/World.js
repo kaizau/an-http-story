7c1a29c7b12930cffcdabb175105be505793e538
Kai Zau <k@kaizau.com>
2020-08-21 11:44:00 +0800

    Add WebXR helper

--- site/game/World.js
+++ site/game/World.js
@@ -21,23 +21,20 @@ export default class World {
     }
 
     const scene = new BABYLON.Scene(this.engine);
-    const camera = new BABYLON.FreeCamera(
-      "camera1",
-      new BABYLON.Vector3(0, 5, -10),
-      scene
-    );
-    camera.setTarget(BABYLON.Vector3.Zero());
-    camera.attachControl(this.canvas, true);
-    const light = new BABYLON.HemisphericLight(
-      "light1",
-      new BABYLON.Vector3(0, 1, 0),
-      scene
-    );
-    light.intensity = 0.7;
+
     if (level.type === "chase") {
       this.createBoxScene(scene, level);
     } else if (level.type === "smash") {
       this.createSphereScene(scene, level);
+    }
+
+    scene.createDefaultCameraOrLight(true, true, true);
+    const env = scene.createDefaultEnvironment();
+    env.setMainColor(new BABYLON.Color3(0.05, 0.2, 0.4));
+    if (window.navigator.xr) {
+      scene.createDefaultXRExperienceAsync({ useMultiview: true });
+    } else {
+      console.log("No WebXR support");
     }
 
     await scene.whenReadyAsync();
@@ -53,18 +50,5 @@ export default class World {
   createSphereScene(scene) {
     const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {}, scene);
     sphere.position.y = 1;
-  }
-
-  // TODO
-  // this.env = scene.createDefaultEnvironment();
-  // this.enableWebXR()
-  async enableWebXR() {
-    if (window.navigator.xr) {
-      await this.scene.createDefaultXRExperienceAsync({
-        floorMeshes: [this.env.ground],
-      });
-    } else {
-      console.log("No WebXR support");
-    }
   }
 }
