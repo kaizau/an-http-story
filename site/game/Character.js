5052cc66523fe35eb26ba30029cece5b61e4621e
Kai Zau <k@kaizau.com>
2020-08-22 08:23:18 +0800

    Extract character, apply movement camera

---
+++ site/game/Character.js
@@ -0,0 +1,52 @@
+const { MeshBuilder, Mesh, Vector3 } = BABYLON;
+
+export default class Character {
+  constructor(game) {
+    this.game = game;
+    this.createLow();
+  }
+
+  // Just a head
+  createLow() {
+    const body = MeshBuilder.CreateBox("character", {
+      height: 1,
+      width: 0.25,
+      depth: 0.15,
+    });
+    body.position.y = 0.75;
+
+    // TODO Better head shape?
+    // https://www.babylonjs-playground.com/#21QRSK#15
+    const head = MeshBuilder.CreateBox("head", {
+      height: 0.2,
+      width: 0.15,
+      depth: 0.1,
+    });
+    head.position.y = 1.5;
+
+    this.mesh = Mesh.MergeMeshes([body, head], true);
+    this.mesh.checkCollisions = true;
+    this.mesh.applyGravity = true; // TODO Necessary?
+    this.mesh.ellipsoid = new Vector3(0.9, 0.45, 0.9); // TODO Adjust
+    this.mesh.speed = new Vector3(0, 0, 0);
+    this.mesh.nextspeed = new Vector3.Zero();
+  }
+
+  // Textures, effects
+  createMedium() {
+    this.createLow();
+  }
+
+  // Head, body, hands
+  createHigh() {
+    this.createLow();
+    this.createMedium();
+  }
+
+  // Face?
+  createBest() {
+    this.createLow();
+    this.createMedium();
+    this.createHigh();
+  }
+}
--- site/game/World.js
+++ site/game/World.js
@@ -1,7 +1,22 @@
+import Character from "./character";
+
+const {
+  Engine,
+  Scene,
+  Vector3,
+  Color3,
+  HemisphericLight,
+  DirectionalLight,
+  ShadowGenerator,
+  MeshBuilder,
+  FollowCamera,
+  Axis,
+} = BABYLON;
+
 export default class World {
   constructor() {
     this.canvas = document.getElementById("canvas");
-    this.engine = new BABYLON.Engine(this.canvas, true);
+    this.engine = new Engine(this.canvas, true);
 
     window.addEventListener("resize", () => this.engine.resize());
 
@@ -20,12 +35,23 @@ export default class World {
       this.scene.dispose();
     }
 
-    const scene = new BABYLON.Scene(this.engine);
+    const scene = new Scene(this.engine);
+    scene.collisionsEnabled = true;
+    // TODO How to actually enable gravity? No Ammo.js in this instance?
+    scene.gravity = new Vector3(0, -9.81, 0);
+
+    // TODO Separate player floor and "platformer" floor
+    const env = scene.createDefaultEnvironment();
+    env.setMainColor(new Color3(0.05, 0.2, 0.4));
+
+    this.createLighting(scene);
+    this.createFloor(scene);
+    this.createCharacter(scene);
+    this.createCamera(scene);
     this.createControls(scene);
-    this.createEnvironment(scene);
 
     if (level.type === "chase") {
-      this.createBoxScene(scene, level);
+      this.createSphereScene(scene, level);
     } else {
       this.createSphereScene(scene, level);
     }
@@ -35,34 +61,68 @@ export default class World {
     this.scene = scene;
   }
 
-  createEnvironment(scene) {
+  createLighting(scene) {
     // TODO Diffuse and specular colors
-    const hemi = new BABYLON.HemisphericLight(
+    const hemi = new HemisphericLight(
       "ambientLight",
-      new BABYLON.Vector3(-0.5, -3, 0),
+      new Vector3(-0.5, -3, 0),
       scene
     );
-    hemi.intensity = 0.8;
 
-    const directional = new BABYLON.DirectionalLight(
+    const directional = new DirectionalLight(
       "directionalLight",
-      new BABYLON.Vector3(-0.5, -3, -1),
+      new Vector3(-0.5, -3, -1),
       scene
     );
     directional.intensity = 0.8;
 
-    const shadowGenerator = new BABYLON.ShadowGenerator(96, directional);
+    const shadowGenerator = new ShadowGenerator(96, directional);
     shadowGenerator.usePoissonSampling = true;
-
-    const env = scene.createDefaultEnvironment();
-    env.setMainColor(new BABYLON.Color3(0.05, 0.2, 0.4));
+    shadowGenerator.darkness = 0.5;
   }
 
-  createControls(scene) {
-    scene.createDefaultCamera(true, true, true);
-    scene.activeCamera.useAutoRotationBehavior = true;
-    scene.activeCamera.beta = Math.PI / 3;
-    scene.activeCamera.radius = 6;
+  createFloor(scene) {
+    // https://www.babylonjs-playground.com/#21QRSK#15
+    const tile = MeshBuilder.CreateBox(
+      "tile",
+      {
+        height: 0.1,
+        width: 10,
+        depth: 2,
+      },
+      scene
+    );
+    tile.position.y = 0;
+    tile.receiveShadows = true;
+    tile.checkCollisions = true;
+  }
+
+  createCharacter(scene) {
+    this.character = new Character(this.game);
+    scene.addMesh(this.character.mesh);
+    const light = scene.lights[scene.lights.length - 1];
+    const shadowGenerator = light.getShadowGenerator();
+    shadowGenerator.addShadowCaster(this.character.mesh);
+  }
+
+  // TODO Reuse VR camera once created
+  createCamera(scene) {
+    const character = this.character.mesh;
+
+    const camera = new FollowCamera(
+      "FollowCam",
+      new Vector3(
+        character.position.x,
+        character.position.y + 5,
+        character.position.z - 20
+      ),
+      scene
+    );
+    camera.cameraDirection = new Vector3(-1, 0, 0);
+    camera.cameraAcceleration = 0;
+    camera.maxCameraSpeed = 20;
+    camera.attachControl(this.canvas, true);
+    camera.lockedTarget = character;
 
     if (window.navigator.xr) {
       scene.createDefaultXRExperienceAsync({ useMultiview: true });
@@ -71,19 +131,121 @@ export default class World {
     }
   }
 
-  createBoxScene(scene) {
-    const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
-    box.position.y = 1;
-    const light = scene.lights[scene.lights.length - 1];
-    const shadowGenerator = light.getShadowGenerator();
-    shadowGenerator.addShadowCaster(box);
+  createControls(scene) {
+    const character = this.character.mesh;
+
+    // Keypress events
+    window.keyisdown = {};
+    window.addEventListener("keydown", function (event) {
+      window.keyisdown[event.keyCode] = true;
+    });
+
+    window.addEventListener("keyup", function (event) {
+      window.keyisdown[event.keyCode] = false;
+    });
+
+    window.tempv = new Vector3.Zero();
+
+    scene.registerBeforeRender(function () {
+      // character speed
+      var v = 0.5;
+      character.nextspeed.x = 0.0;
+      character.nextspeed.z = 0.00001;
+      if (window.keyisdown[37]) {
+        character.nextspeed.x = -v;
+        character.nextspeed.z = -v;
+      }
+      if (window.keyisdown[39]) {
+        character.nextspeed.x = v;
+        character.nextspeed.z = v;
+      }
+      if (window.keyisdown[38]) {
+        character.nextspeed.x = -v;
+        character.nextspeed.z = v;
+      }
+      if (window.keyisdown[40]) {
+        character.nextspeed.x = v;
+        character.nextspeed.z = -v;
+      }
+
+      character.speed = new Vector3.Lerp(
+        character.speed,
+        character.nextspeed,
+        0.1
+      );
+
+      // Turn to direction
+      // if (character.speed.length() > 0.01) {
+      //   tempv.copyFrom(character.speed);
+      //   var dot = new Vector3.Dot(tempv.normalize(), Axis.Z);
+      //   var al = Math.acos(dot);
+      //   if (tempv.x < 0.0) {
+      //     al = Math.PI * 2.0 - al;
+      //   }
+      //   if (window.keyisdown[9]) {
+      //     console.log("dot,al:", dot, al);
+      //   }
+      //   if (al > character.rotation.y) {
+      //     var t = Math.PI / 30;
+      //   } else {
+      //     var t = -Math.PI / 30;
+      //   }
+      //   var ad = Math.abs(character.rotation.y - al);
+      //   if (ad > Math.PI) {
+      //     t = -t;
+      //   }
+      //   if (ad < Math.PI / 15) {
+      //     t = 0;
+      //   }
+      //   character.rotation.y += t;
+      //   if (character.rotation.y > Math.PI * 2) {
+      //     character.rotation.y -= Math.PI * 2;
+      //   }
+      //   if (character.rotation.y < 0) {
+      //     character.rotation.y += Math.PI * 2;
+      //   }
+      // }
+
+      character.moveWithCollisions(character.speed);
+
+      if (character.position.x > 60.0) {
+        character.position.x = 60.0;
+      }
+      if (character.position.x < -60.0) {
+        character.position.x = -60.0;
+      }
+      if (character.position.z > 60.0) {
+        character.position.z = 60.0;
+      }
+      if (character.position.z < -60.0) {
+        character.position.z = -60.0;
+      }
+    });
   }
 
   createSphereScene(scene) {
-    const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {}, scene);
+    const sphere = MeshBuilder.CreateSphere("sphere", {}, scene);
     sphere.position.y = 1;
+    sphere.position.x = 1.5;
+    sphere.position.z = 2;
     const light = scene.lights[scene.lights.length - 1];
     const shadowGenerator = light.getShadowGenerator();
     shadowGenerator.getShadowMap().renderList.push(sphere);
   }
 }
+
+// Unlikely to fit within 13k limit... :(
+// importMesh(scene) {
+//   SceneLoader.ImportMesh(
+//     "",
+//     "meshes/",
+//     "cat.gltf",
+//     scene,
+//     (meshes, particleSystems, skeletons) => {
+//       const cat = meshes[0];
+//       cat.position.x = 1;
+//       cat.position.z = -0.5;
+//       scene.beginAnimation(skeletons[0], 0, 100, true);
+//     }
+//   );
+// }
